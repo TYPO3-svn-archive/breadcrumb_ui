ee5e902904cc9b6aa8b30af8404480281b7155f8
sivakumar.bdu <sivakumar.bdu@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-08-30 09:34:02 +0000

    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/breadcrumb_ui/branches/org@51530 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ breadcrumb_ui/ext_tables.sql
@@ -0,0 +1,7 @@
+#
+# Table structure for table 'pages'
+#
+CREATE TABLE pages (
+	tx_breadcrumbui_breadcrumb_hide tinyint(3) DEFAULT '0' NOT NULL,
+	tx_breadcrumbui_breadcrumb_unlink tinyint(3) DEFAULT '0' NOT NULL
+);
