7d8e1795603b9fbd996fad933aadeea44bf12da3
sivakumar.bdu <sivakumar.bdu@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-08-30 09:30:04 +0000

    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/breadcrumb_ui/branches/org@51529 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ breadcrumb_ui/ext_emconf.php
@@ -0,0 +1,44 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "breadcrumb_ui".
+#
+# Auto generated 30-08-2011 13:45
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Easy Breadcrumb',
+	'description' => 'Extension used for displaying breadcrumb, easy to configure. User can hide or unlink the breadcrumb on specific pages
+. This extension extends pages table.',
+	'category' => 'fe',
+	'author' => 'Sivakumar, SPRITLE Software',
+	'author_email' => 'sivakumarv@spritle.com',
+	'shy' => '',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'beta',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '0.0.0',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+);
+
+?>
